0716303e55f4ef1860ecc7e5eb7f3eb658191df0
Yanis <yanis.blanchard@gmail.com>
2025-11-02 15:17:03 +0100

    feat: add shadcn/ui Button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello from the index page</h1>
+    <div>
+      <Button>Hello from shadcn/ui</Button>
+      <h1>hello from the index page</h1>
+    </div>
   );
 }
